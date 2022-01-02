cfae580dc094e6e921de73c6c3b2286045f4e5e3
Andrew Yu <andrewyu209@gmail.com>
2022-01-02 18:58:16 -0500

    Added category-routes Sequelize API

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
     through: ProductTag,
-    as: 'product_tag',
+    as: 'tags',
     foreignKey: 'tag_id'
 });
 
